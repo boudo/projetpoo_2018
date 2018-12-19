0be2420d3634ac960c3c34f9c8b2ff9e1b794b93
Aziz EL ASSRI <aziz.el-assri@ens.uvsq.fr>
2018-12-19 13:12:23 +0100

    Ajout des tests unitaires sur Creature

--- rogueLike/src/test/java/fr/uvsq/uvsq21603700/rogueLike/AppTest.java
+++ rogueLike/src/test/java/fr/uvsq/uvsq21603700/rogueLike/AppTest.java
@@ -1,12 +1,39 @@
 package fr.uvsq.uvsq21603700.rogueLike;
 
+import org.junit.*;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+
+import java.awt.Color;
 
 public class AppTest {
+	Terrain[][] t = new Terrain[800][600] ;
+	  World w1 = new World(t);
+	  Creature c1 =new Creature(w1,'f',"adversaire",Color.BLUE,60,20,50);
+	  Creature c2 =new Creature(w1,'y',"adversaire",Color.YELLOW,40,30,50);
   @Test
   public void testApp() {
     assertTrue(true);
+	  assertNotNull(c1);
+	  
   }
+  @Test
+  public void testMaxVie() {
+	  assertEquals(60,c1.getMaxVie());
+  }
+  @Test
+  public void testDeplacement() {
+	  c1.x=10;c1.y=20;
+	 c1.deplacer(100, 50);
+	 assertEquals(110,c1.x);
+	 assertEquals(70,c1.y);
+  }
+  /*
+  @Test
+  public void test1{
+		  Terrain[][] t ;
+	  World w1 = new World(t);
+  Creature c=new Creature(w1,'f',"adversaire",Color.BLUE,60,20,50);
+} */
 }
